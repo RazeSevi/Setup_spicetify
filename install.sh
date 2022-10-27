baae596f844b562eb52a0c7837f28096cee98046
Christophe De Beule <christophe19991234@gmail.com>
2022-10-27 17:03:48 +0200

    Changed the set_theme script

--- install.sh
+++ install.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+NC='\033[0m' 
 
-echo "Installing repo packages"
-echo ""
-git clone https://github.com/RazeSevi/spicetify-themes.git
-echo ""
-echo "installing spicetify"
-echo ""
-yay -S spicetify-cli
-echo ""
-echo "Done"
+if ! [[ -d spicetify-themes ]]; then
 
+	echo -e "${GREEN}Installing repo packages${NC}"
+	echo ""
+	git clone https://github.com/RazeSevi/spicetify-themes.git
+	echo ""
+else
+	echo -e "${GREEN}Repo packages are already installed.${NC}"
+fi
+
+if ! yay -Q spicetify-cli 2>/dev/null 1>/dev/null; then
+	echo -e"${GREEN}installing spicetify${NC}"
+	echo ""
+	yay -S spicetify-cli
+	echo ""
+	echo -e "${GREEN}Done${NC}"
+else
+	echo -e "${GREEN}Spicetify-cli is already installed.${NC}"
+fi
 cd spicetify-themes
 cp -r * ~/.config/spicetify/Themes
